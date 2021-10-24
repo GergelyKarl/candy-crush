f937067842b1ecac100afc3b1b3ff40aa4bc3f9c
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-10-24 19:26:33 +0200

    0.2 check matches columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,62 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import { colors, width } from "./data/data";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  const [randomColors, setRandomColors] = useState<Array<String>>([] as Array<String>);
+
+  const createBoard = (): void => {
+    const randomizedColors: string[] = [];
+    for (let i = 0; i < width ** 2; i++) {
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
+      randomizedColors.push(randomColor);
+    }
+
+    setRandomColors(randomizedColors);
+  };
+
+  const checkMatchesForThree = () => {
+    for (let i = 0; i < randomColors.length; i++) {
+      const columnsMatched = [i, i + width, i + width * 2];
+      const matchedColor = randomColors[i];
+      if (columnsMatched.every((color) => randomColors[color] === matchedColor)) {
+        columnsMatched.forEach((number) => (randomColors[number] = ""));
+      }
+    }
+  };
+  const checkMatchesForFour = () => {
+    for (let i = 0; i < randomColors.length; i++) {
+      const columnsMatched = [i, i + width, i + width * 2,i+width*3];
+      const matchedColor = randomColors[i];
+      if (columnsMatched.every((color) => randomColors[color] === matchedColor)) {
+        columnsMatched.forEach((number) => (randomColors[number] = ""));
+      }
+    }
+  };
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  useEffect(() => {
+    const myInterval = setInterval(() => {
+      checkMatchesForThree();
+      checkMatchesForFour()
+      setRandomColors([...randomColors]);
+    }, 100);
+    return () => clearInterval(myInterval);
+  }, [checkMatchesForThree,randomColors,checkMatchesForFour]);
+
+  console.log(randomColors);
+
+  return (
+    <>
+      <div className="app">
+        <div className="board">
+          {randomColors.map((colorName, index) => (
+            <img key={index} style={{ backgroundColor: `${colorName}` }} alt="color name" />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default App;
---
+++ src/data/data.ts
@@ -0,0 +1,2 @@
+export const colors = ["red", "blue", "green", "orange", "yellow", "purple"];
+export const width=8
